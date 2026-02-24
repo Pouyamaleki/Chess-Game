f4fc2d11694a54ebf88db6b3f9a4d8d387aae393
Pouya <pouyamaleki85@gmail.com>
2026-02-24 22:57:05 +0330

    add Rule header file

---
+++ Include/Rule/Rules.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#include "GamePosition.h"
+#include "Move.h"
+
+class GamePosition;
+
+class Rules {
+public:
+    // Core rule checking
+    static bool isLegalMove(const GamePosition& pos,
+                            const Position& from,
+                            const Position& to);
+
+    static void applyMove(GamePosition& pos,
+                          const Position& from,
+                          const Position& to);
+
+    // State checks
+    static bool isCheck(const GamePosition& pos, Color color);
+    static bool isCheckmate(const GamePosition& pos, Color color);
+    static bool isStalemate(const GamePosition& pos, Color color);
+
+    // Move generation
+    static std::vector<Move> generateLegalMoves(const GamePosition& pos,
+                                                Color color);
+
+private:
+    // Helpers
+    static Position findKing(const Board& board, Color color);
+    static bool squareAttacked(const Board& board,
+                               const Position& square,
+                               Color byColor);
+
+    static bool wouldBeInCheckAfterMove(const GamePosition& pos,
+                                        const Position& from,
+                                        const Position& to);
+};
