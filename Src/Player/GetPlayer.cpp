950c12158a96f0c2ced94462face396680a0b9cd
Pouya <pouyamaleki85@gmail.com>
2026-02-24 22:51:27 +0330

    add Get Player cpp file

---
+++ Src/Player/GetPlayer.cpp
@@ -0,0 +1,42 @@
+#include "GetPlayer.h"
+
+//constructor 
+GetPlayer::GetPlayer(string name, Color color) : Player(name, color)
+{
+}
+
+// geting move
+Move GetPlayer::getMove(const Board &board)
+{
+    char fromFile, toFile;
+    int fromRank, toRank;
+
+    while (true)
+    {
+        cout << endl;
+        cout << getName() << " turn (" << (getColor() == Color::White ? "White" : "Black") << ")" << endl;
+        cout << "Enter move (e.g., e2 e4): ";
+
+        if (cin >> fromFile >> fromRank >> toFile >> toRank)
+        {
+            if (fromFile >= 'a' && fromFile <= 'h' &&
+                toFile >= 'a' && toFile <= 'h' &&
+                fromRank >= 1 && fromRank <= 8 &&
+                toRank >= 1 && toRank <= 8)
+            {
+                break;
+            }
+        }
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input! Please use format like: e2 e4" << endl;
+    }
+
+    int fromCol = fromFile - 'a';
+    int fromRow = 8 - fromRank;
+    int toCol = toFile - 'a';
+    int toRow = 8 - toRank;
+
+    return Move(Position(fromRow, fromCol), Position(toRow, toCol));
+}
