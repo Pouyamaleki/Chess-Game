cb7b1f6578d12e04958a125324651e0540612669
Pouya <pouyamaleki85@gmail.com>
2026-02-24 22:58:02 +0330

    add build folder with saves.txt file

---
+++ build/CMakeFiles/Chess.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/codes/C++/chess++/Src/Board/Board.cpp" "CMakeFiles/Chess.dir/Src/Board/Board.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Board/Board.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Event/EventManager.cpp" "CMakeFiles/Chess.dir/Src/Event/EventManager.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Event/EventManager.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Event/MassacreAbillity.cpp" "CMakeFiles/Chess.dir/Src/Event/MassacreAbillity.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Event/MassacreAbillity.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Event/Mission.cpp" "CMakeFiles/Chess.dir/Src/Event/Mission.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Event/Mission.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Event/SeasonEvent.cpp" "CMakeFiles/Chess.dir/Src/Event/SeasonEvent.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Event/SeasonEvent.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Event/TimeBomb.cpp" "CMakeFiles/Chess.dir/Src/Event/TimeBomb.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Event/TimeBomb.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Game/Game.cpp" "CMakeFiles/Chess.dir/Src/Game/Game.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Game/Game.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Game/GameModeManager.cpp" "CMakeFiles/Chess.dir/Src/Game/GameModeManager.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Game/GameModeManager.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/GamePosition/GamePosition.cpp" "CMakeFiles/Chess.dir/Src/GamePosition/GamePosition.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/GamePosition/GamePosition.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/GameStorage/GameStorage.cpp" "CMakeFiles/Chess.dir/Src/GameStorage/GameStorage.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/GameStorage/GameStorage.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Move/Move.cpp" "CMakeFiles/Chess.dir/Src/Move/Move.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Move/Move.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/ArmoredQueen.cpp" "CMakeFiles/Chess.dir/Src/Piece/ArmoredQueen.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/ArmoredQueen.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Bishop.cpp" "CMakeFiles/Chess.dir/Src/Piece/Bishop.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Bishop.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Joker.cpp" "CMakeFiles/Chess.dir/Src/Piece/Joker.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Joker.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/King.cpp" "CMakeFiles/Chess.dir/Src/Piece/King.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/King.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Knight.cpp" "CMakeFiles/Chess.dir/Src/Piece/Knight.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Knight.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Pawn.cpp" "CMakeFiles/Chess.dir/Src/Piece/Pawn.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Pawn.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Piece.cpp" "CMakeFiles/Chess.dir/Src/Piece/Piece.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Piece.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Queen.cpp" "CMakeFiles/Chess.dir/Src/Piece/Queen.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Queen.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Rook.cpp" "CMakeFiles/Chess.dir/Src/Piece/Rook.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Rook.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/SpecialPawn.cpp" "CMakeFiles/Chess.dir/Src/Piece/SpecialPawn.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/SpecialPawn.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Piece/Spy.cpp" "CMakeFiles/Chess.dir/Src/Piece/Spy.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Piece/Spy.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Player/GetPlayer.cpp" "CMakeFiles/Chess.dir/Src/Player/GetPlayer.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Player/GetPlayer.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Player/Player.cpp" "CMakeFiles/Chess.dir/Src/Player/Player.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Player/Player.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Player/Watcher.cpp" "CMakeFiles/Chess.dir/Src/Player/Watcher.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Player/Watcher.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/Rule/Rules.cpp" "CMakeFiles/Chess.dir/Src/Rule/Rules.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/Rule/Rules.cpp.obj.d"
+  "D:/codes/C++/chess++/Src/main/main.cpp" "CMakeFiles/Chess.dir/Src/main/main.cpp.obj" "gcc" "CMakeFiles/Chess.dir/Src/main/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
